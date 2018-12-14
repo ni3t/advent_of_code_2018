8b5291732aa8ee9ec077909f46c4777f41c7ce81
Nick Bell <ni3t@2015mbp.local>
2018-12-14 12:21:53 -0500

    update 14

--- 14/solution.rb
+++ 14/solution.rb
@@ -6,12 +6,11 @@ START = 3710
 @elves = [0,1]
 
 20_000_000.times do
-    elf_scores = [@scores[@elves[0]],@scores[@elves[1]]]
-    @scores.concat(elf_scores.inject(&:+).to_s.chars.map(&:to_i))
-    @elves = @elves.each_with_index.map do |elf,i|
-        new_pos = (elf + 1 + elf_scores[i]) % @scores.length
-        new_pos
-    end
+  elf_scores = [@scores[@elves[0]],@scores[@elves[1]]]
+  @scores.concat(elf_scores.inject(&:+).to_s.chars.map(&:to_i))
+  @elves = @elves.each_with_index.map do |elf,i|
+    new_pos = (elf + 1 + elf_scores[i]) % @scores.length
+  end
 end
 
 
@@ -20,16 +19,16 @@ pp @scores[INPUT...(INPUT+10)].map(&:to_s).join
 
 @cur_idx = 0
 until @matched || @cur_idx >= 100
-    str = @scores[0...1e9].join
-    if str.match(/236021/)
-        idx = @cur_idx * 1_000_000
-        @matched = true
-        idx += str.index /236021/ 
-        puts idx
-    else
-        nil
-    end
-    @cur_idx += 1
-    @scores.rotate!(1e9)
+  str = @scores[0...1e9].join
+  if str.match(/236021/)
+    idx = @cur_idx * 1_000_000
+    @matched = true
+    idx += str.index /236021/ 
+    puts idx
+  else
+    nil
+  end
+  @cur_idx += 1
+  @scores.rotate!(1e9)
 end
 
